8475f8960f48fbb5dccbf9871f8674b5335f2912
francesca-giorno <francescagiorno1996@gmail.com>
2020-11-20 15:08:07 +0100

    add js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 const todolist = new Vue({
-  
-})
+  el:"#root",
+  data:{
+    inputs: [],
+    text:"",
+
+  },
+
+  methods: {
+   addText: function() {
+      inputs.push(this.text)
+  },
+   removeText: function() {
+    inputs.splice(this.text)
+    }
+  }
+
+  })
